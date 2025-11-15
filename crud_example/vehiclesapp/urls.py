9753341a749aad6903f40a9c1f843a035ffc979f
stheban <stheban.hoyos@campusucc.edu.co>
2025-11-14 20:37:00 -0500

    Delete crud_example directory

--- crud_example/vehiclesapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home_view, create_view, list_view, update_view, delete_view
-
-urlpatterns = [
-    path('create/', create_view, name='create'),
-    path('', list_view, name='read'),
-    path('update/<int:id>/', update_view, name='update'),
-    path('delete/<int:id>/', delete_view, name='delete'),
-]
